68fc3d4b9962c25364f806780d5fa89e6bfc5805
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-20 08:57:58 +0300

    Lection3

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,7 +1,35 @@
 ﻿/* Заменить все пробелы черточками, все мал буквы к большими К
 большие С - маленькими с*/
 
-string text = "- Я думаю, -сказал князь, улыбаясь, - что, "
-            + "ежели ба вас послаи вместо нашего милого Винценгероде,"
+string text = "- Я думаю, -Сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
             + "вы бы взяли приступом согласие прусского короля. "
             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s="qwerty"
+//           012
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
